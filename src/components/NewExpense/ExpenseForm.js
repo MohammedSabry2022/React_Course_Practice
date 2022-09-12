7f6171dc044d563fcc58f7e1541835a45d0a1dd3
mohammedsabry2022 <mohammedsabry2022@hotmail.com>
2022-09-12 09:13:55 +0200

    Conditional Content

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -33,43 +33,48 @@ const ExpenseForm = (props) => {
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enterdTitle}
-            onChange={titleChangeHandler}
-          ></input>
-        </div>
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enterdTitle}
+              onChange={titleChangeHandler}
+            ></input>
+          </div>
 
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enterdAmount}
-            onChange={amountChangeHandler}
-          ></input>
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2022-01-01"
-            max="2022-12-31"
-            value={enterdDate}
-            onChange={dateChangeHandler}
-          ></input>
-        </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enterdAmount}
+              onChange={amountChangeHandler}
+            ></input>
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2022-01-01"
+              max="2022-12-31"
+              value={enterdDate}
+              onChange={dateChangeHandler}
+            ></input>
+          </div>
 
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
+          <div className="new-expense__actions">
+            <button type="button" onClick={props.onCancel}>
+              Cancel
+            </button>
+            <button type="submit">Add Expense</button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
